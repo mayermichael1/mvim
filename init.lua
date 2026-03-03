66b56aebef6cffbf8927e102b04b55ac78158fee
mayermichael1 <michael.mayer2105@gmail.com>
2026-03-03 21:54:30 +0100

    note enabled for now

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
 -- this file is empty
 -- everything is sourced from the plugin/ directory
+
+-- project local settings
+-- TODO: these options will load before plugins. does not really make sense in that case
+-- this setting needs to be here
+-- vim.opt.exrc = true
+
