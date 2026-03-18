a97ad8e05f2bc4c5216704ece5f6b5d58d0da8b6
mayermichael1 <michael.mayer2105@gmail.com>
2026-03-18 13:25:39 +0100

    show colorcolumns on first few indents

--- lua/prosa_settings.lua
+++ lua/prosa_settings.lua
@@ -5,3 +5,12 @@ vim.opt_local.textwidth = 80
 -- spell checking (german default)
 vim.opt_local.spell = true
 vim.opt_local.spelllang = "de"
+
+-- colorcolumns for prosa text
+local columns= {};
+table.insert(columns, vim.bo.textwidth);
+for i = 0,2 
+do
+    table.insert(columns, 1 + i * vim.bo.shiftwidth);
+end
+vim.opt_local.colorcolumn = columns
