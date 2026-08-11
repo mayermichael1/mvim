1a799818c6049b69240086ab3897f624c655a374
mayermichael1 <michael.mayer2105@gmail.com>
2026-08-11 22:18:13 +0200

    project local settings are enabled

--- init.lua
+++ init.lua
@@ -4,5 +4,5 @@
 -- project local settings
 -- TODO: these options will load before plugins. does not really make sense in that case
 -- this setting needs to be here
--- vim.opt.exrc = true
+vim.opt.exrc = true
 
